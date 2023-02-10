fd9e4de79ca94a63a7db2eb808d4f17b06754d68
tato <jason.ma1904@gmail.com>
2023-02-10 12:19:09 +1100

    fix: update dom render to React18 createRoot/client

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
 
-import App from './App';
-import './index.css';
+import App from "./App";
+import "./index.css";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
